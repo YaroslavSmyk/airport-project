3716a9022405404665346a7c9ff33d2babd3251b
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-10-19 11:03:41 +0300

    work then/ async-await

--- src/gateway/Gateway.js
+++ src/gateway/Gateway.js
@@ -3,21 +3,22 @@ import moment from 'moment';
 const baseUrl = 'https://api.iev.aero/api/flights';
 // let currentDate = moment(new Date()).format('DD-MM-YYYY');
 
-export const fetchFlightList = (currentDate) => {
-  fetch(`${baseUrl}/${currentDate}`).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error('Server Error');
-  })
-  // .then(response => response.filter(el => moment(el.localtime).format('DD-MM-YYYY') === currentDate))
-  .catch(err => alert(err.message))
-}
-
-// export const fetchFlightList = async(date) => {
-//  const response = await fetch(baseUrl)
-//     if (response.ok) {
-//       return response.json();
+// export const fetchFlightList = (currentDate) => {
+//   fetch(`${baseUrl}/${currentDate}`).then((res) => {
+//     if (res.ok) {
+//       return res.json();
 //     }
-//     return [];
-//   };
+//     throw new Error('Server Error');
+//   })
+//   // .then(response => response.filter(el => moment(el.localtime).format('DD-MM-YYYY') === currentDate))
+//   .catch(err => alert(err.message))
+// }
+
+
+export const fetchFlightList = async(currentDate) => {
+ const response = await fetch(`${baseUrl}/${currentDate}`)
+    if (response.ok) {
+      return response.json();
+    }
+    return [];
+  };
